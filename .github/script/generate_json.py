b61113c100e66ec7f0cf26ea046e3fa314f2584b
cdc <96666783+cdc-sys@users.noreply.github.com>
2024-03-06 16:09:16 +0100

    Update generate_json.py

--- .github/script/generate_json.py
+++ .github/script/generate_json.py
@@ -6,5 +6,5 @@ for file in f_arr:
   file_s = str(file)
   file_s2 = file_s.strip(".png")
   content.append(file_s2)
-json_dict = {"thumbnails":content}
+json_dict = {"level_ids":content}
 print(json.dumps(json_dict))
