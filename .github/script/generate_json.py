1022a34d0e7a9ba453c1b781caf30f41e3ba674d
cdc <96666783+cdc-sys@users.noreply.github.com>
2024-03-06 15:34:27 +0100

    update json script

--- .github/script/generate_json.py
+++ .github/script/generate_json.py
@@ -1,4 +1,9 @@
 import os
 
-arr = os.listdir("./thumbs")
-print(arr)
+f_arr = os.listdir("./thumbs")
+content = []
+for file in f_arr:
+  file_s = str(file)
+  file_s2 = file_s.strip(".png")
+  content.push(file_s2)
+print(content)
