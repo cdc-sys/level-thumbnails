0a71dcb638f680e33bcb8f172c0b3705a6b199db
cdc <96666783+cdc-sys@users.noreply.github.com>
2024-03-06 16:08:47 +0100

    ACTUAL json

--- .github/script/generate_json.py
+++ .github/script/generate_json.py
@@ -1,9 +1,10 @@
 import os
-
+import json
 f_arr = os.listdir("./thumbs")
 content = []
 for file in f_arr:
   file_s = str(file)
   file_s2 = file_s.strip(".png")
   content.append(file_s2)
-print(content)
+json_dict = {"thumbnails":content}
+print(json.dumps(json_dict))
