36d772b389f9634ac9e575d7dc322cd1481c9ace
cdc <96666783+cdc-sys@users.noreply.github.com>
2024-03-26 11:32:45 +0100

    Update generate_json.py

--- .github/script/generate_json.py
+++ .github/script/generate_json.py
@@ -1,10 +1,12 @@
 import os
 import json
+import time
+
 f_arr = os.listdir("./thumbs")
 content = []
 for file in f_arr:
   file_s = str(file)
   file_s2 = file_s.strip(".png")
   content.append(file_s2)
-json_dict = {"level_ids":content}
+json_dict = {"lastUpdated": int(time.time()), "level_ids": content}
 print(json.dumps(json_dict,indent=4))
